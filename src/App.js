4386527e5aeef1a0f9fe0f27eaeecda5baee246f
Saif-anime <saif817883@gmail.com>
2025-02-20 00:00:35 +0530

    React Video 11 Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Card from './comp/Card'
+import Navbar from './comp/Navbar'
+import Footer from './comp/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <>
+
+        <Navbar />
+        <div className='container mx-auto '>
+          <div className='gap-10 grid grid-cols-4 my-10 mx-10'>
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            
+
+          </div>
+        </div>
+        <Footer />
+
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
