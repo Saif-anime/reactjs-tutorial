30d985a0f5ecc7f426f33fd03500a84f039de3d3
Saif-anime <saif817883@gmail.com>
2025-02-20 02:05:52 +0530

    React 12 Video ShopCart

--- src/App.js
+++ src/App.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react'
 import Card from './comp/Card'
 import Navbar from './comp/Navbar'
 import Footer from './comp/Footer'
+import Sidebar from './comp/Sidebar'
+
+
 
 export class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hide:true
+    }
+
+
+    // binding 
+// this.handleSidebar = this.handleSidebar.bind(this)
+
+
+
+  }
+
+
+  handleSidebar =() =>{
+
+
+    if(this.state.hide){
+      this.setState({hide:false});
+    }else{
+      this.setState({hide:true})
+    }
+  }
+
+
+
+
   render() {
     return (
       <>
 
-        <Navbar />
+
+{/* function props  */}
+        <Navbar handleSidebar={this.handleSidebar}/>
+
+        <Sidebar Hide={this.state.hide} handleSidebar={this.handleSidebar}/>
+
+
         <div className='container mx-auto '>
           <div className='gap-10 grid grid-cols-4 my-10 mx-10'>
 
@@ -19,11 +58,19 @@ export class App extends Component {
             <Card />
             <Card />
             <Card />
-            
+
 
           </div>
+
+
+
+
+
         </div>
         <Footer />
+
+
+
 
       </>
     )
